954970d574f897f399f9bd502fc8299533e45d04
lmoranglez <luciamoranglez@gmail.com>
2025-03-02 20:39:12 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pyoset",
+    name="easiq_marcus",
     version="0.1",
-    author="Lucía Morán-González, Albert Solé-Daura",  
-    py_modules=['4points', 'get_info', 'pyosethermo', 'simpledft_marcus','utilities'],
+    author="Lucía Morán-González, Albert Solé-Daura, Feliu Maseras",  
+    py_modules=['4points', 'get_info', 'get_results','utilities'],
     packages=find_packages(),
     install_requires=['pyssian @ git+https://github.com/maserasgroup-repo/pyssian.git@develop', 
                       'pyssianutils @ git+https://github.com/maserasgroup-repo/pyssian-utils.git'] # Add dependencies if needed
